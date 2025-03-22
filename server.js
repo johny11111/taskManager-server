b5a0bebe7dbcc4207694f12f74d86619682e676f
johny11111 <125285264+johny11111@users.noreply.github.com>
2025-03-22 03:34:04 +0200

    cors

--- server.js
+++ server.js
@@ -20,18 +20,20 @@ const allowedOrigins = [
     'https://taskmanager-client-2pyw.onrender.com'
   ];
   
-  app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); // מאשר את הבקשה
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+  // app.use(cors({
+  //   origin: function (origin, callback) {
+  //     if (!origin || allowedOrigins.includes(origin)) {
+  //       callback(null, true); // מאשר את הבקשה
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   allowedHeaders: ['Content-Type', 'Authorization']
+  // }));
   
+  app.use(cors());
+
 
 app.use(express.json());
 
